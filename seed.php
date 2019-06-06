bd7b89321a3c0a266cea587ab266d1b8d36443ca
mijkami <dimitri.gaillot@gmail.com>
2019-06-06 10:34:31 +0200

    update faker, supporting name

--- seed.php
+++ seed.php
@@ -5,12 +5,11 @@ require('./login.php');
 $faker = Faker\Factory::create('fr_FR');
 
 //CONNEXION à la base de données : https://www.php.net/manual/fr/pdo.connections.php
-
 $bdd = new PDO('mysql:host=localhost;dbname=Sorty', $user, $pass);
 $dbh->exec('DELETE FROM `Users`');
 //insert en mode PDO : https://php.net/manual/fr/pdo.prepared-statements.php
 
-$stmt = $dbh->prepare("INSERT INTO Users (phone, firstname, email) VALUES (:phone, :firstname, :email)");
+$stmt = $dbh->prepare( "INSERT INTO Users (phone, name, firstname, email) VALUES (:phone, :name, :firstname, :email)");
 $stmt->bindParam(':phone', $phone);
 $stmt->bindParam(':name', $name);
 $stmt->bindParam(':firstname', $firstname);
@@ -18,6 +17,7 @@ $stmt->bindParam(':email', $email);
 
 for ($i = 0; $i < 5; $i++){
     $phone = $faker->phoneNumber;
+    $name = $faker->name;
     $firstname = $faker->firstName;
     echo $firstname;
     $email = $faker->unique()->email;

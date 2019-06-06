ecaa2c96a09c795cba8381e74375c6610cca5847
mijkami <dimitri.gaillot@gmail.com>
2019-06-06 10:44:03 +0200

    fix dbh / bdd call line 8, 9 & 12

--- seed.php
+++ seed.php
@@ -6,10 +6,10 @@ $faker = Faker\Factory::create('fr_FR');
 
 //CONNEXION à la base de données : https://www.php.net/manual/fr/pdo.connections.php
 $bdd = new PDO('mysql:host=localhost;dbname=Sorty', $user, $pass);
-$dbh->exec('DELETE FROM `Users`');
+$bdd->exec('DELETE FROM `Users`');
 //insert en mode PDO : https://php.net/manual/fr/pdo.prepared-statements.php
 
-$stmt = $dbh->prepare( "INSERT INTO Users (phone, name, firstname, email) VALUES (:phone, :name, :firstname, :email)");
+$stmt = $bdd->prepare( "INSERT INTO Users (phone, name, firstname, email) VALUES (:phone, :name, :firstname, :email)");
 $stmt->bindParam(':phone', $phone);
 $stmt->bindParam(':name', $name);
 $stmt->bindParam(':firstname', $firstname);

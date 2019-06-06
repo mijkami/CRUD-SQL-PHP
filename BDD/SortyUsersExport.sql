6cac8f9a4b8104810bba6d18b83149a068895b7c
mijkami <dimitri.gaillot@gmail.com>
2019-06-06 10:07:51 +0200

    add BDD

---
+++ BDD/SortyUsersExport.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jun 06, 2019 at 09:52 AM
+-- Server version: 5.7.26-0ubuntu0.19.04.1
+-- PHP Version: 7.2.17-0ubuntu0.19.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Sorty`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `firstname` char(40) NOT NULL,
+  `phone` char(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`id`, `email`, `name`, `firstname`, `phone`) VALUES
+(0, 'sorty0@yopmail.com', 'Austrich', 'Antoine', '003040506070'),
+(1, 'sorty2@yopmail.com', 'Bolty', 'Bernard', '013040506070'),
+(2, 'sorty2@yopmail.com', 'Crafty', 'Chantal', '023040506070'),
+(3, 'sort31@yopmail.com', 'Duston', 'Denis', '033040506070');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
